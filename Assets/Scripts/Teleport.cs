8c3b3bb5389030a09c303cd140b97ab3f6c6e76d
Andrew Lauer <andrewcl@email.unc.edu>
2024-12-12 21:25:54 -0500

    inter-station teleporting fixed

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -3,14 +3,17 @@ using UnityEngine;
 public class TeleportPlayer : MonoBehaviour
 {
     public Transform teleportDestination; // Where the player will be teleported
+    public GameObject playerCamera;
 
     void OnTriggerEnter(Collider other)
     {
         // Check if the object entering the trigger is the player
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("GolfClub"))
         {
+            Debug.Log("Info: Golf club has entered the collider (" + other + ")");
             // Teleport the player to the destination
-            other.transform.position = teleportDestination.position;
+            //other.transform.position = teleportDestination.position;
+            playerCamera.transform.position = teleportDestination.position;
 
             // Optional: Reset player velocity if using a Rigidbody
             Rigidbody rb = other.GetComponent<Rigidbody>();
